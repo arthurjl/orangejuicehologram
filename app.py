cfc9cbb5906a13a4a27d15b35c6341bcf696a26f
arthurliu <theonlyarthurliu@gmail.com>
2020-08-15 20:55:14 -0700

    incorporate upload with demo site HAHAHAHAHAHA

--- app.py
+++ app.py
@@ -21,7 +21,50 @@ app.config["FILE_UPLOADS"] = "uploads/"
 app.config["ALLOWED_FILE_UPLOAD_EXTENSIONS"] = ["PNG", "JPG", "PDF", "JPEG", "MP4"]
 app.config["MAX_FILESIZE"] = 5 * 1024 * 1024
 
-@app.route('/upload', methods=['POST', 'GET'])
+# @app.route('/upload', methods=['POST', 'GET'])
+# def upload():
+#     if request.method == "POST":
+#         if request.files:
+#             video = request.files["video"]  # Matches 'name' attribute
+#             if not is_filesize_allowed(request.cookies.get('filesize')):
+#                 print(f"filesize exceeds limit of {app.config['MAX_FILESIZE']}")
+#                 return redirect(request.url)
+#             elif not is_filename_safe(video.filename):
+#                 print("filename is not safe / allowed")
+#                 return redirect(request.url)
+#             filename = secure_filename(video.filename)
+#             upload_location = os.path.join(app.config["FILE_UPLOADS"], filename)
+#             print(f"Uploading file to {upload_location}")
+#             upload_blob(video, upload_location)
+#             return redirect(request.url)
+
+#     # Else, GET request
+#     return render_template('demo.html')
+
+
+def is_filename_safe(filename):
+    if filename == "":
+        print("File must have a filename")
+        return False
+    elif not "." in filename:
+        print("File does not have extensions")
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    return ext.upper() in app.config["ALLOWED_FILE_UPLOAD_EXTENSIONS"]
+
+def is_filesize_allowed(filesize):
+    if not filesize:
+        print('Cookies must be enabled to support upload (for our security purposes)')
+        return False
+    return int(filesize) < app.config["MAX_FILESIZE"]
+
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/demo', methods=['POST', 'GET'])
 def upload():
     if request.method == "POST":
         if request.files:
@@ -39,31 +82,6 @@ def upload():
             return redirect(request.url)
 
     # Else, GET request
-    tasks = [{'name': 'get stuff done', 'detail': 'yes yes yes'}, {'name': 'get stuff don1e', 'detail': 'yes yes y2es'}, {'name': 'g3et stuff done', 'detail': 'yes 4yes yes'},]
-    return render_template('upload.html', tasks=tasks)
-
-
-def is_filename_safe(filename):
-    if filename == "":
-        print("File must have a filename")
-        return False
-    elif not "." in filename:
-        print("File does not have extensions")
-        return False
-
-    ext = filename.rsplit(".", 1)[1]
-
-    return ext.upper() in app.config["ALLOWED_FILE_UPLOAD_EXTENSIONS"]
-
-def is_filesize_allowed(filesize):
-    return int(filesize) < app.config["MAX_FILESIZE"]
-
-@app.route('/about', methods=['POST', 'GET'])
-def about():
-    return render_template('about.html')
-
-@app.route('/demo', methods=['POST', 'GET'])
-def demo():
     return render_template('demo.html')
 
 
