235dc5ed02d3eedf5a7627402e99154d4f4d27f4
Jake Jung <junyong217@hotmail.com>
2020-08-16 01:08:58 -0700

    Updated video converter to include middle border

--- backend/video_converter.py
+++ backend/video_converter.py
@@ -1,4 +1,4 @@
-from moviepy.editor import CompositeVideoClip, VideoFileClip, clips_array, vfx
+from moviepy.editor import ColorClip, CompositeVideoClip, VideoFileClip, clips_array, vfx
 
 # Converts video into three-dimensional projection format
 def convertVideo(filepath, percentage):
@@ -15,15 +15,23 @@ def convertVideo(filepath, percentage):
     clip3 = clip1.rotate(180)
     clip4 = clip1.rotate(270)
 
+    # Retrieve clip of white border for placement of cone
+    clip5 = ColorClip(size=(400, 400), color=(255, 255, 255))
+    clip6 = ColorClip(size=(390, 390), color=(0, 0, 0))
+
     # Calculate clip positions
     clip1_pos = ((width / 2) - (clip_width / 2), 0)
     clip2_pos = (0, (width / 2) - (clip_width / 2))
     clip3_pos = ((width / 2) - (clip_width / 2), width - clip_height) 
     clip4_pos = (width - clip_height, (width / 2) - (clip_width / 2))
+    clip5_pos = ((width / 2) - 200, (width / 2) - 200)
+    clip6_pos = ((width / 2) - 195, (width / 2) - 195)
 
     # Produce composition of clips
     final_clip = CompositeVideoClip([clip1.set_position(clip1_pos), 
                                     clip2.set_position(clip2_pos), 
                                     clip3.set_position(clip3_pos), 
-                                    clip4.set_position(clip4_pos)], size=(width, width))
+                                    clip4.set_position(clip4_pos),
+                                    clip5.set_position(clip5_pos).set_duration(clip1.duration),
+                                    clip6.set_position(clip6_pos).set_duration(clip1.duration)], size=(width, width))
     final_clip.write_videofile(filepath.replace(".", "_converted."))
